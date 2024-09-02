0bc081a86c5cb0700d592893fac0c684fef81c12
Rakesh Kumar <rakeshk@techfoursolutions.com>
2024-09-02 14:00:45 +0530

    Replaced single quote to double quote

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ String.prototype.isQuestion = function isQuestion() {
 
 String.prototype.hasVowels = function hasVowels() {
   // Returns true if a vowel exists in the input string. Returns false otherwise.
-  const vowels = new RegExp('[aeiou]', 'i');
+  const vowels = new RegExp("[aeiou]", 'i');
   return vowels.test(this);
 };
 
